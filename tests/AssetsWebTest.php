3567bc82cf06df0e14a6c324e0c0e91b58eb07fd
Mochamad Gufron <mgufronefendi@gmail.com>
2013-07-12 07:21:48 +0700

    fixing app

--- tests/AssetsWebTest.php
+++ tests/AssetsWebTest.php
@@ -8,7 +8,7 @@ class AssetsServiceProviderTest extends WebTestCase
 {
 	public function createApplication()
 	{
-		$app = require(__DIR__.'/../app.php');
+		$app = require(__DIR__.'/app.php');
 		$app['debug'] = true;
 		$app['exception_handler']->disable();
 		return $app;
